60c84d8e859c8054529aaf5df94fbbad32090a11
Biplab Sharma <jobs.biplab.sharma@gmail.com>
2023-09-07 00:04:57 +0530

    Add project files.

---
+++ TestService/ProjectInstaller.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace TestService
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+        }
+
+        private void serviceInstaller1_AfterInstall(object sender, InstallEventArgs e)
+        {
+            try
+            {
+                ServiceInstaller serviceInstaller = (ServiceInstaller)sender;
+                using (ServiceController sc = new ServiceController(serviceInstaller.ServiceName))
+                {
+                    sc.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+               
+            }
+        }
+    }
+}
